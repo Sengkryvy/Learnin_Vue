fe71b3366aef6aad8918db2e17b4fd9969aca4f4
Sengkryvy <sengkryvy@gmail.com>
2021-02-01 18:01:52 +0700

    01-02-21

--- VueJS/index.js
+++ VueJS/index.js
@@ -23,18 +23,73 @@ new Vue({
         y: 0
     },
     methods: {
-        increaseAge:function(num = 1) {
-            this.age += num;
+        increaseAge:function() {
+            this.age ++;
+            console.log("increaseAge ran.");
         },
         decreaseAge:function() {
             this.age--;
+            console.log("decreaseA ran.");
+
         },
         updateXY: function(event) {
             this.x = event.offsetX;
             this.y = event.offsetY;
+            console.log("updateXY ran.");
+        }
+    },
+
+});
+
+//Computed Properties
+new Vue({
+    el: "#computed-properties",
+    data: {
+        a: 0,
+        b: 0,
+        x: 0,
+    },
+    methods: {
+        increaseA_fn: function() {
+            this.a ++;
+            console.log("increaseA_fn ran");
+        },
+        increaseB_fn: function() {
+            this.b ++;
+            console.log("increaseB_fn ran");
+        },
+        xPlusA_fn: function() {
+            console.log("xPlusA_fn ran");
+            return parseInt(this.x) + parseInt(this.a);
+        
+        },
+        xPlusB_fn: function() {
+            console.log("xPlusB_fn ran");
+            return parseInt(this.x) + parseInt(this.b);
+        }
+    },
+    computed: {
+        increaseA: function() {
+            this.a++;
+            console.log("increaseA ran.");
+        },
+        increaseB: function() {
+            this.b++;
+            console.log("increaseB ran.");
+
+        },        
+        xPlusA: function() {
+            console.log("xPlusA ran");
+            return parseInt(this.x) + parseInt(this.a);
+        },
+        xPlusB: function() {
+            console.log("xPlusB ran");
+            return parseInt(this.x) + parseInt(this.b);
         }
     }
 });
 
-//Computed Properties
+// Dynamic CSS Class
+new Vue({
 
+});
